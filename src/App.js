b2790ac888ae6b912fdefad410b776b369a0be47
Francisco <fgallegosgo@gmail.com>
2020-05-20 16:32:25 -0600

    add form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { Fragment } from "react";
+import Form from './components/Form';
+
 
 
 function App() {
   return (
-    <h1>Patient Manager</h1>
+    <Fragment>
+      <h1>Patient Manager</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+              <Form />
+          </div>
+          <div className="one-half column">
+              2   
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
