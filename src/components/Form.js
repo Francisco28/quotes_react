29d6736efe7e2de49b05ce0bc5636f6110541eff
Francisco <fgallegosgo@gmail.com>
2020-05-20 17:58:52 -0600

    event to read the content of the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,21 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Form = () => {
+
+    //create "State" of quotes
+    const [quote, updateQuote] = useState({
+        pet: '',
+        owner: '',
+        date: '',
+        hour: '',
+        symptoms: ''
+    });
+
+    //function that execute every that an user write in a input
+    const updateState = () => {
+        console.log('writing...');
+    }
+
   return (
     <Fragment>
       <h2>Create quote</h2>
@@ -12,6 +27,7 @@ const Form = () => {
           name="pet"
           className="u-full-width"
           placeholder="Pet's name"
+          onChange={updateState}
         />
 
         <label>Owner's name</label>
@@ -20,6 +36,7 @@ const Form = () => {
           name="owner"
           className="u-full-width"
           placeholder="Owner's name"
+          onChange={updateState}
         />
 
         <label>Date</label>
@@ -27,6 +44,7 @@ const Form = () => {
             type="date" 
             name="date" 
             className="u-full-width" 
+            onChange={updateState}
         />
 
         <label>Time</label>
@@ -34,12 +52,14 @@ const Form = () => {
             type="time" 
             name="hour" 
             className="u-full-width" 
+            onChange={updateState}
         />
 
         <label>Symptoms</label>
         <textarea
             className="u-full-width"
             name="symptoms"
+            onChange={updateState}
         ></textarea>
 
         <button
