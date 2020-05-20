9c85a42e6fd1bff3de401820bb844ecba61df65e
Francisco <fgallegosgo@gmail.com>
2020-05-20 16:57:59 -0600

    form of new quote

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
 const Form = () => {
-    return ( <h1>From the form</h1> );
-}
- 
+  return (
+    <Fragment>
+      <h2>Create quote</h2>
+
+      <form>
+        <label>Pet's name</label>
+        <input
+          type="text"
+          name="pet"
+          className="u-full-width"
+          placeholder="Pet's name"
+        />
+
+        <label>Owner's name</label>
+        <input
+          type="text"
+          name="owner"
+          className="u-full-width"
+          placeholder="Owner's name"
+        />
+
+        <label>Date</label>
+        <input 
+            type="date" 
+            name="date" 
+            className="u-full-width" 
+        />
+
+        <label>Time</label>
+        <input 
+            type="time" 
+            name="hour" 
+            className="u-full-width" 
+        />
+
+        <label>Symptoms</label>
+        <textarea
+            className="u-full-width"
+            name="symptoms"
+        ></textarea>
+
+        <button
+            type="submit"
+            className="u-full-width button-primary"
+        >Add Quote</button>
+      </form>
+    </Fragment>
+  );
+};
+
 export default Form;
